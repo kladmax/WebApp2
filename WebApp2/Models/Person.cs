19de796a528df225e5302eb64134ea5df433f697
kladmax <51287678+kladmax@users.noreply.github.com>
2021-03-04 16:56:56 +0200

    Add project files.

---
+++ WebApp2/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace WebApp2.Models
+{
+    /// <summary>
+    /// Человек
+    /// </summary>
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
