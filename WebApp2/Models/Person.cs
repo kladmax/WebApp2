9bee1f46b795d29a2600c2be5d18bca3e5abb34e
kladmax <51287678+kladmax@users.noreply.github.com>
2021-03-04 18:00:03 +0200

    добавил комментарий, убрал пустые строки

--- WebApp2/Models/Person.cs
+++ WebApp2/Models/Person.cs
@@ -1,12 +1,14 @@
-﻿
-
-namespace WebApp2.Models
+﻿namespace WebApp2.Models
 {
     /// <summary>
     /// Человек
     /// </summary>
     public class Person
     {
+
+        /// <summary>
+        /// описание полей ID для баз данных
+        /// </summary>
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
